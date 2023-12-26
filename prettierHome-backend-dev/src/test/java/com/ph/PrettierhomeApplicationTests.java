577230d87108d486a03f5ec7b32509e9561bf723
Kenan KILIC <96704212+Klcknn@users.noreply.github.com>
2023-12-27 00:14:36 +0300

    add files

--- prettierHome-backend-dev/src/test/java/com/ph/PrettierhomeApplicationTests.java
+++ prettierHome-backend-dev/src/test/java/com/ph/PrettierhomeApplicationTests.java
@@ -3,7 +3,7 @@ package com.ph;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {PrettierhomeApplicationTests.class})
 class PrettierhomeApplicationTests {
 
 	@Test
